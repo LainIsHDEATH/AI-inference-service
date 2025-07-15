7bf700cc5ad8488a84ad486b1c506f29db3f528f
LainIsHDEATH <ivan.shankin04@gmail.com>
2025-07-15 19:36:17 +0300

    Small fixes (DB, CORS)

--- app/main.py
+++ app/main.py
@@ -7,3 +7,16 @@ app = FastAPI(lifespan=lifespan)
 
 app.include_router(lstm_router, prefix="/lstm", tags=["lstm"])
 app.include_router(rl_router, prefix="/rl", tags=["rl"])
+
+from fastapi.middleware.cors import CORSMiddleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("app.main:app", host="0.0.0.0", port=7003, reload=True)
